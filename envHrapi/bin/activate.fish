f336016b1d55ad2ffabe9a4760e300b455f0cf49
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2014-10-06 02:15:07 +0300

    - fixed virtualenv for opensuse and added tar file

--- envHrapi/bin/activate.fish
+++ envHrapi/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Gabriel/Documents/workspaces/pyHrapi/envHrapi"
+set -gx VIRTUAL_ENV "/home/gabriel/Repositories/envHrapi"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
